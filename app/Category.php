70c7cd23c0f6b4c43ac14ff910e9217e175e6fee
Petro Ostapuk <petroostapuk@gmail.com>
2018-01-21 19:43:47 +0200

    Added all methods for Category controller and set all views

--- app/Category.php
+++ app/Category.php
@@ -3,11 +3,32 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Category extends Model
 {
+    use Sluggable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title'
+    ];
+
     public function posts()
     {
         return $this->hasMany(Post::class);
+    }
+
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
